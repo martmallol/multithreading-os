c6e914adb528d7b2427637c8a9d442d37ba613b5
Martin Mallol <martinmallolcc@gmail.com>
2022-10-09 12:53:41 -0300

    Punto y coma faltante

--- definiciones.h
+++ definiciones.h
@@ -5,6 +5,6 @@ using namespace std;
 enum direccion {ARRIBA=0,ABAJO,IZQUIERDA,DERECHA};
 typedef pair<int, int> coordenadas;
 enum color {AZUL=0,ROJO,INDEFINIDO,VACIO,EMPATE,BANDERA_ROJA,BANDERA_AZUL};
-enum estrategia {SECUENCIAL=0,RR,SHORTEST,USTEDES}
+enum estrategia {SECUENCIAL=0,RR,SHORTEST,USTEDES};
 
-#endif /* DEFINICIONES_H */
+#endif // DEFINICIONES_H
